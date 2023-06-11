f40b1fb35a2a56053fd0640db8d03d95e0c0d22a
Niels <niels.rogge1@gmail.com>
2023-06-11 21:19:04 +0200

    Add print statements

--- modeling/backbone/vit.py
+++ modeling/backbone/vit.py
@@ -402,15 +402,15 @@ class ViT(Backbone):
 
         for i, blk in enumerate(self.blocks):
 
-            if i == 0:
-                print("Shape of x before layer 0:", x.shape)
-                print("First values of x before layer 0:", x[0, 0, :3, :3])
+            if i == 2:
+                print("Shape of x before layer 2:", x.shape)
+                print("First values of x before layer 2:", x[0, 0, :3, :3])
 
             x = blk(x)
 
-            if i == 0:
-                print("Shape of x after layer 0:", x.shape)
-                print("First values of x after layer 0:", x[0, 0, :3, :3])
+            if i == 2:
+                print("Shape of x after layer 2:", x.shape)
+                print("First values of x after layer 2:", x[0, 0, :3, :3])
 
         outputs = {self._out_features[0]: x.permute(0, 3, 1, 2)}
 
